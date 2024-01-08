c4ee53fd1ee6c20d03f4d0fbee9bf27a50ebcd04
Andrzej Rafalski <anraf1001@gmail.com>
2024-01-08 20:46:53 +0100

    Fix formatting

--- scripts/optitrack_sim_node.py
+++ scripts/optitrack_sim_node.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from optitrack_sim.msg import OptiData 
+from optitrack_sim.msg import OptiData
 from tf_transformations import quaternion_from_euler
 import time
 import yaml
@@ -12,39 +12,41 @@ import socket
 UDP_IP = "192.168.110.2"
 UDP_PORT = 12111
 
-class MinimalPublisher(Node):
 
+class MinimalPublisher(Node):
     def __init__(self):
-        super().__init__('optitrack_simulator')
-        self.twist_publisher_ = self.create_publisher(Twist, '/optitrack/pose', 10)  
+        super().__init__("optitrack_simulator")
+        self.twist_publisher_ = self.create_publisher(Twist, "/optitrack/pose", 10)
         self.receive_from_server()
-        
+
     def receive_from_server(self):
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        sock.bind((UDP_IP,UDP_PORT))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, UDP_PORT))
 
         while True:
-            data,addr = sock.recvfrom(1024)
-            decoded_data = data.decode('utf-8')
-            
+            data, addr = sock.recvfrom(1024)
+            decoded_data = data.decode("utf-8")
+
             try:
                 received_object = yaml.safe_load(decoded_data)
             except:
                 continue
-                
-            if not isinstance(received_object,dict) or 'rigid_bodies' not in received_object:
+
+            if (
+                not isinstance(received_object, dict)
+                or "rigid_bodies" not in received_object
+            ):
                 continue
-            
-            if not received_object['rigid_bodies']:
-                continue 
-            
-            
-            rigid_bodies = [float(i) for i in received_object['QDrone']]
-            
+
+            if not received_object["rigid_bodies"]:
+                continue
+
+            rigid_bodies = [float(i) for i in received_object["QDrone"]]
+
             if not rigid_bodies[0]:
                 print("Drone not in cage!")
                 continue
-            
+
             message = Twist()
             # Position
             message.linear.x = rigid_bodies[1]
@@ -68,5 +70,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
